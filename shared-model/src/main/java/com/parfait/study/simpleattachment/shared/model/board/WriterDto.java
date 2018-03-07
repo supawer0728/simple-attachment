fd781837b99caf208433d2cbb5657b106528bee0
supawer0728 <supawer0728@gmail.com>
2018-03-08 01:27:11 +0900

    writer 추가 리팩토링

--- shared-model/src/main/java/com/parfait/study/simpleattachment/shared/model/board/WriterDto.java
+++ shared-model/src/main/java/com/parfait/study/simpleattachment/shared/model/board/WriterDto.java
@@ -1,9 +1,10 @@
 package com.parfait.study.simpleattachment.shared.model.board;
 
+import com.parfait.study.simpleattachment.shared.model.attachment.Attachment;
 import lombok.Data;
 
 @Data
-public class WriterDto {
+public class WriterDto implements Attachment {
     private Long id;
     private String username;
     private String email;
--- web/src/main/java/com/parfait/study/simpleattachment/attachment/service/writer/AttachWriterToBoardService.java
+++ web/src/main/java/com/parfait/study/simpleattachment/attachment/service/writer/AttachWriterToBoardService.java
@@ -1,36 +1,40 @@
 package com.parfait.study.simpleattachment.attachment.service.writer;
 
 import com.parfait.study.simpleattachment.attachment.service.AttachService;
+import com.parfait.study.simpleattachment.shared.model.attachment.Attachable;
 import com.parfait.study.simpleattachment.shared.model.attachment.AttachmentType;
-import com.parfait.study.simpleattachment.shared.model.attachment.WriterAttachable;
+import com.parfait.study.simpleattachment.shared.model.board.BoardDto;
 import com.parfait.study.simpleattachment.shared.model.board.WriterDto;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WriterAttachService implements AttachService<WriterAttachable> {
+public class AttachWriterToBoardService implements AttachService<BoardDto> {
 
+    private static final AttachmentType supportAttachmentType = AttachmentType.WRITER;
+    private static final Class<BoardDto> supportType = BoardDto.class;
     private final WriterClient writerClient;
-    private static final Class<WriterAttachable> supportType = WriterAttachable.class;
 
-    public WriterAttachService(@NonNull WriterClient writerClient) {
+    @Autowired
+    public AttachWriterToBoardService(@NonNull WriterClient writerClient) {
         this.writerClient = writerClient;
     }
 
     @Override
     public AttachmentType getSupportAttachmentType() {
-        return AttachmentType.WRITER;
+        return supportAttachmentType;
     }
 
     @Override
-    public Class<WriterAttachable> getSupportType() {
+    public Class<BoardDto> getSupportType() {
         return supportType;
     }
 
     @Override
-    public void attach(Object attachment) {
-        WriterAttachable writerAttachable = supportType.cast(attachment);
-        WriterDto writerDto = writerClient.getWriterByBoardId(writerAttachable.getWriterAttachableTargetId());
-        writerAttachable.attachWriter(writerDto);
+    public void attach(Attachable attachment) {
+        BoardDto boardDto = supportType.cast(attachment);
+        WriterDto writerDto = writerClient.getWriterByBoardId(boardDto.getId());
+        attachment.attach(supportAttachmentType, writerDto);
     }
 }
