ac3cc3713ca276b9b7b0e6fe5e26d37958d3cf44
Jihoon.Jeon <jihoon.jeon@nhnent.com>
2018-03-08 12:27:10 +0900

    BoardDto 수정

--- shared-model/src/main/java/com/parfait/study/simpleattachment/shared/model/board/BoardDto.java
+++ shared-model/src/main/java/com/parfait/study/simpleattachment/shared/model/board/BoardDto.java
@@ -1,7 +1,15 @@
 package com.parfait.study.simpleattachment.shared.model.board;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.parfait.study.simpleattachment.shared.model.attachment.Attachment;
+import com.parfait.study.simpleattachment.shared.model.attachment.AttachmentType;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -9,4 +17,8 @@ public class BoardDto {
     private Long id;
     private String title;
     private String content;
+
+    @Setter(AccessLevel.PRIVATE)
+    @JsonIgnore
+    private Map<AttachmentType, Attachment> attachmentMap = new EnumMap<>(AttachmentType.class);
 }
